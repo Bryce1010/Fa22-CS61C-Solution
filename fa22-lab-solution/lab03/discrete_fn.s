618f47fcf729401655b8e56eaf4c23de3c49f587
linjing.yx <linjing.yx@alibaba-inc.com>
2023-01-29 14:25:30 +0800

    fa22-lab3-solution

--- fa22-lab-solution/lab03/discrete_fn.s
+++ fa22-lab-solution/lab03/discrete_fn.s
@@ -5,7 +5,9 @@
 # a1 is the address of the "output" array (defined above).
 # The return value should be stored in a0
 f:
-
+    slli a2, a0, 2
+    add a2, a2, a1
+    lw a0, 12(a2)
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra  
